7997b3eac60b0018ba93b10133838bd641f26871
1151297588@qq.com <1151297588@qq.com>
2018-04-19 15:31:43 +0800

    simple drawing

--- pages/game/game.js
+++ pages/game/game.js
@@ -1,25 +1,30 @@
 // pages/game/game.js
+const ctx = wx.createCanvasContext('myCanvas')
+var x, y
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+    ctx.setLineWidth(6) // 设置线宽
+    ctx.setLineCap('round') //设置线条的端点样式
+    ctx.setStrokeStyle('#000000') //描边样式
+    ctx.setFillStyle('#000000') //填充样式
   },
   
   /**
@@ -62,5 +67,29 @@ Page({
    */
   onShareAppMessage: function () {
   
+  },
+
+  //坐标
+  start: function (e) {
+    x = e.touches[0].x
+    y = e.touches[0].y
+    
+  },
+  move: function (e) {
+
+    ctx.moveTo(x, y) // 设置路径起点坐标
+    x = e.touches[0].x
+    y = e.touches[0].y
+    ctx.lineTo(x, y) // 绘制一条直线
+    ctx.stroke()
+    ctx.draw(true)
+
+  },
+  end: function (e) {
+    
+    ctx.arc(x ,y , 3, 0, 2 * Math.PI)//圆点
+    ctx.fill()
+    ctx.draw(true)
   }
+
 })
